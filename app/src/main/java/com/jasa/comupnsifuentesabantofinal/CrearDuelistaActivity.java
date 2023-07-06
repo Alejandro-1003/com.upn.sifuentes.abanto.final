60289233333055a9942e3633fc6f595bb68506c4
Alejandro Sifuentes <Sifuentesabanto03@gmail.com>
2023-07-06 08:00:34 -0500

    commit 1

---
+++ app/src/main/java/com/jasa/comupnsifuentesabantofinal/CrearDuelistaActivity.java
@@ -0,0 +1,14 @@
+package com.jasa.comupnsifuentesabantofinal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CrearDuelistaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crear_duelista);
+    }
+}
--- app/src/main/java/com/jasa/comupnsifuentesabantofinal/MainActivity.java
+++ app/src/main/java/com/jasa/comupnsifuentesabantofinal/MainActivity.java
@@ -3,6 +3,8 @@ package com.jasa.comupnsifuentesabantofinal;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button crear,ver,sincronizar;
+        crear=findViewById(R.id.botCrearDuelista);
+        ver=findViewById(R.id.botVerDuelistas);
+        sincronizar=findViewById(R.id.botSincronizarDuelistas);
+
+        crear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        ver.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        sincronizar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
